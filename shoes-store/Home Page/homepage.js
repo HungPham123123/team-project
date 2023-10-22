813c66f9bd09f131684a5a0b2758c5a7a4dc2d81
HungPham123123 <129242472+HungPham123123@users.noreply.github.com>
2023-10-22 22:51:32 +0700

    update homepage

---
+++ shoes-store/Home Page/homepage.js
@@ -0,0 +1,11 @@
+$('.image-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+  });
+          
