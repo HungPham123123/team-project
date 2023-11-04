2191f05c67c096d6ac113b20319bff24f1856c77
HungPham123123 <129242472+HungPham123123@users.noreply.github.com>
2023-11-04 20:15:24 +0700

    responsive

--- shoes-store/Home Page/homepage.js
+++ shoes-store/Home Page/homepage.js
@@ -7,5 +7,70 @@ $('.image-slider').slick({
     autoplay: true,
     autoplaySpeed: 3000,
     arrows: false,
+    responsive: [
+      {
+        breakpoint: 1446,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 1144,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      }, 
+      {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    ]
   });
           
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log (entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show')
+    } 
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden')
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+function togglePopup() {
+  document.getElementById("popup-1").classList.toggle("active")
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const quantityInput = document.getElementById('quantityInput');
+  const qtyMinus = document.querySelector('.qty-minus');
+  const qtyPlus = document.querySelector('.qty-plus');
+
+  qtyMinus.addEventListener('click', function () {
+      // Get the current quantity value
+      let currentValue = parseInt(quantityInput.value);
+      
+      // Ensure the value doesn't go below 1
+      if (currentValue > 1) {
+          quantityInput.value = currentValue - 1;
+      }
+  });
+
+  qtyPlus.addEventListener('click', function () {
+      // Get the current quantity value
+      let currentValue = parseInt(quantityInput.value);
+      
+      // Increment the value
+      quantityInput.value = currentValue + 1;
+  });
+});
